0e6ba05b588223ec225e9ac8eff74e52ad75c19e
davi.ribeiro <davi.ribeiro@ibti.org.br>
2020-09-22 17:57:12 -0300

    ajust

--- src/router.js
+++ src/router.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Router, Route, Switch, Redirect } from 'react-router-dom'
 import { history } from './component'
 
-import AppCotent from './pages/AppContent'
 import Viagens from './pages/viagens/viagens'
 
 export default () => (
@@ -10,7 +9,6 @@ export default () => (
     <Switch>
       <Route path='/' exact={true} component={Viagens} />
       <Route path='/viagens' exact={true} component={Viagens} />
-      <Route path='/hoteis' exact={true} component={AppCotent} />
       <Redirect from='*' to='/' />
     </Switch>
   </Router>
